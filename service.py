7c566643f28587d5becd1015a599691ec6421119
Ian Ooi <ionaic@gmail.com>
2014-07-26 00:57:46 -0700

    Submission streamlined to redirect user to homepage after form submission

--- service.py
+++ service.py
@@ -10,23 +10,16 @@ data_fields = ("placeholder")
 def graph():
     return send_file('templates/index.html')
 
-@app.route("/form")
+@app.route("/form", methods=['GET', 'POST'])
 def form():
-    #if request.method == 'POST':
-    #    if all([k in request.form for k in vote_fields]):
-    #        submit_vote(*[request.form[k] for k in vote_fields])
-    #        return send_file('templates/index.html')
-    #else:
-    return send_file('templates/form.html')
-
-@app.route("/data", methods=['GET', 'POST'])
-def data():
     if request.method == 'POST':
         if all([k in request.form for k in vote_fields]):
             submit_vote(*[request.form[k] for k in vote_fields])
             return send_file('templates/index.html')
-    else:
-        5
+    return send_file('templates/form.html')
+
+@app.route("/data")
+def data():
     return send_file('graph.json')
 
 if __name__ == "__main__":
